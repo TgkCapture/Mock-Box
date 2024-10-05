754127cb07551a02add9abfadf4d2b37fba4b7d4
TgkCapture <tawongakanyenda5@gmail.com>
2024-10-05 11:42:23 +0000

    feat: mock up server that simulates phones numbers at an end point.

---
+++ mock_server.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+import random
+import configparser
+import os
+
+app = Flask(__name__)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+@app.route('/api/phone-numbers', methods=['GET'])
+def get_phone_numbers():
+    phone_numbers = generate_mock_phone_numbers()
+    return jsonify(phone_numbers)
+
+def generate_mock_phone_numbers():
+    # Simulating a list of phone numbers
+    return [
+        "+265881124753",
+        "+265881124753",
+        "+265881124753",
+        "+265881124753",
+        "+265881124753"
+    ]
+
+@app.route('/api/error', methods=['GET'])
+def error_simulation():
+    return jsonify({"error": "Simulated error"}), 500
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data_type = request.args.get('type', 'default')
+    if data_type == 'numbers':
+        return jsonify(generate_mock_phone_numbers())
+    else:
+        return jsonify({"message": "Unknown data type"}), 400
+
+@app.after_request
+def log_request(response):
+    app.logger.info(f'{request.method} {request.path} - {response.status_code}')
+    return response
+
+if __name__ == '__main__':
+    port = int(config['server']['port'])
+    debug_mode = config['server'].getboolean('debug')
+    
+    app.run(port=port, debug=debug_mode)
