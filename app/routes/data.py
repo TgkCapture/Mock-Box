63858224a5b0a11e8f803f1dbd3012087368ff4c
TgkCapture <tawongakanyenda5@gmail.com>
2024-10-29 15:06:00 +0200

    feat: handling different types of data requests

---
+++ app/routes/data.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, request
+from ..utils import generate_mock_phone_numbers
+
+data_bp = Blueprint('data', __name__)
+
+@data_bp.route('/data', methods=['GET'])
+def get_data():
+    data_type = request.args.get('type', 'default')
+    if data_type == 'numbers':
+        return jsonify(generate_mock_phone_numbers())
+    elif data_type == 'names':
+        return jsonify(generate_mock_names())
+    else:
+        return jsonify({"message": "Unknown data type"}), 400
