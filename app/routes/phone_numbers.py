53dcf835bdcd09591736e2d0a37b4af0331c5a8a
TgkCapture <tawongakanyenda5@gmail.com>
2024-10-29 15:04:13 +0200

    feat: hanndling phone number related endpoints

---
+++ app/routes/phone_numbers.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify
+from ..utils import generate_mock_phone_numbers
+
+phone_numbers_bp = Blueprint('phone-numbers', __name__)
+
+@phone_numbers_bp.route('/phone-numbers', methods=['GET'])
+def get_phone_numbers():
+    phone_numbers = generate_mock_phone_numbers()
+    return jsonify(phone_numbers)
